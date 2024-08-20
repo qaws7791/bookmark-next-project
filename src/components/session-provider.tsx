3add73ee7b3707d8d6adccdef06ccea8b7c511a3
qaws7791 <qaws7791@gmail.com>
2024-08-20 23:23:25 +0900

    chore: Add SessionProvider component for managing user sessions

---
+++ src/components/session-provider.tsx
@@ -0,0 +1,50 @@
+"use client";
+import { createClient } from "@/lib/supabase/client";
+import { Session } from "@supabase/supabase-js";
+import { createContext, useContext, useEffect, useState } from "react";
+
+const SessionContext = createContext<Session | null>(null);
+export const useSession = () => {
+  const session = useContext(SessionContext);
+  if (session === null) {
+    throw new Error("useSession must be used within a SessionProvider");
+  }
+  return session;
+};
+
+export default function SessionProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const supabase = createClient();
+  const [loading, setLoading] = useState(true);
+  const [session, setSession] = useState<Session | null>(null);
+
+  useEffect(() => {
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((event, session) => {
+      if (event === "SIGNED_OUT") {
+        setSession(null);
+      } else if (session) {
+        setSession(session);
+      }
+      setLoading(false);
+    });
+
+    return () => {
+      subscription.unsubscribe();
+    };
+  }, [supabase]);
+
+  if (loading) {
+    return null;
+  }
+
+  return (
+    <SessionContext.Provider value={session}>
+      {children}
+    </SessionContext.Provider>
+  );
+}
