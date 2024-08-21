2ab9576ca8b4fc4295d65b559e149c4687563694
qaws7791 <qaws7791@gmail.com>
2024-08-21 23:29:17 +0900

    chore: Add Supabase configuration files and client modules

---
+++ supabase/server/index.ts
@@ -0,0 +1,34 @@
+import { createServerClient } from "@supabase/ssr";
+import { cookies } from "next/headers";
+import "server-only";
+import { Database } from "../supabase";
+
+export function createClient() {
+  const cookieStore = cookies();
+
+  // Create a server's supabase client with newly configured cookie,
+  // which could be used to maintain user's session
+  return createServerClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return cookieStore.getAll();
+        },
+        setAll(cookiesToSet) {
+          try {
+            cookiesToSet.forEach(({ name, value, options }) =>
+              // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+              cookieStore.set(name, value, { ...options }),
+            );
+          } catch {
+            // The `setAll` method was called from a Server Component.
+            // This can be ignored if you have middleware refreshing
+            // user sessions.
+          }
+        },
+      },
+    },
+  );
+}
