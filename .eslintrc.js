2dbd22f7d57840ccc7c38351b047af436e7ecba8
qaws7791 <qaws7791@gmail.com>
2024-08-21 23:29:31 +0900

    chore: Update ESLint configuration to use TypeScript parser and recommended rules

---
+++ .eslintrc.js
@@ -0,0 +1,56 @@
+module.exports = {
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
+  globals: {
+    React: true,
+    JSX: true,
+  },
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: [
+    "next/core-web-vitals",
+    "plugin:unicorn/recommended",
+    "prettier",
+    "plugin:@typescript-eslint/recommended-type-checked",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  ignorePatterns: [
+    "node_modules/",
+    ".next/",
+    "out/",
+    "public/",
+    "*.js",
+    "next-env.d.ts",
+  ],
+  root: true,
+  rules: {
+    "@typescript-eslint/no-misused-promises": [
+      "error",
+      { checksVoidReturn: false },
+    ],
+    "unicorn/prevent-abbreviations": [
+      "error",
+      {
+        replacements: {
+          param: false,
+          params: false,
+          ref: false,
+          props: false,
+        },
+      },
+    ],
+    "unicorn/no-array-reduce": "off",
+    "unicorn/no-null": "off",
+    "unicorn/no-array-for-each": "off",
+  },
+};
